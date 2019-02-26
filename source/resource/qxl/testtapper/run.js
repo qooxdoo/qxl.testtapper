b6d522ee415105cfdf09be1b9a35164d8cd897e9
Tobias Oetiker <tobi@oetiker.ch>
2019-02-26 16:43:08 +0100

    fully promisified runner

--- source/resource/qxl/testtapper/run.js
+++ source/resource/qxl/testtapper/run.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const puppeteer = require("puppeteer");
+let fs = require("fs");
+let puppeteer = require("puppeteer");
 
 let argv = require("yargs")
   .command("$0 [options] <url>", "run a tests with TAP compliant reporting", yargs => {
@@ -29,7 +29,7 @@ let argv = require("yargs")
   })
   .help("h")
   .alias("h", "help")
-  .version("0.1.5")
+  .version("0.1.6")
   .wrap(72)
   .strict(true)
   .argv;
@@ -51,49 +51,47 @@ console.log("CALL "+ href.href);
 
 let notOk = 0;
 let Ok = 0;
-try {
-  (async () => {
-    let browser = await puppeteer.launch();
-    let page = await browser.newPage();
-    page.on("console", async msg => {
-      let args = await msg.args();
-      Promise.all(args.map(async arg => {
-        let val = await arg.jsonValue();
-        // value is serializable
-        // eslint-disable-next-line no-negated-condition
-        if (val.match && JSON.stringify(val) !== JSON.stringify({})) {
-          if  ( val.match(/^\d+\.\.\d+$/)) {
-            browser.close();
-            console.info(`DONE testing ${Ok} ok, ${notOk} not ok`);
-            process.exit(notOk);
-          }
-          if (val.match(/^not ok /)) {
-            notOk++;
-            console.log(val);
-          } else if (val.match(/^ok\s/)) {
-            if (!argv.terse) {
+puppeteer.launch().then(
+  browser => {
+    return browser.newPage().then(
+      page => {
+        page.on("console", msg => {
+          let val = msg.text;
+          // value is serializable
+          // eslint-disable-next-line no-negated-condition
+          if (val.match && JSON.stringify(val) !== JSON.stringify({})) {
+            if  ( val.match(/^\d+\.\.\d+$/)) {
+              browser.close();
+              console.info(`DONE testing ${Ok} ok, ${notOk} not ok`);
+              process.exit(notOk);
+            }
+            if (val.match(/^not ok /)) {
+              notOk++;
+              console.log(val);
+            } else if (val.match(/^ok\s/)) {
+              if (!argv.terse) {
+                console.log(val);
+              }
+              Ok++;
+            } else if (val.match(/^#/) && argv.diag) {
+              console.log(val);
+            } else if (val.verbose) {
               console.log(val);
             }
-            Ok++;
-          } else if (val.match(/^#/) && argv.diag) {
-            console.log(val);
-          } else if (val.verbose) {
-            console.log(val);
+          } else {
+            const {
+              type, subtype, description
+            } = arg._remoteObject;
+            if (val.diag || val.verbose) {
+              console.log(description);
+            }
           }
-        } else {
-          const {
-            type, subtype, description
-          } = arg._remoteObject;
-          if (val.diag || val.verbose) {
-            console.log(description);
-          }
-        }
-      })).catch(err => {
-        throw (err);
-      });
-    });
-    await page.goto(href.href);
-  })();
-} catch (err) {
-  console.log(err);
-}
+        });
+        return page.goto(href.href);
+      }
+    );
+  }
+).catch( err => {
+  console.log(`ERROR ${err}`);
+  process.exit(1);
+});
