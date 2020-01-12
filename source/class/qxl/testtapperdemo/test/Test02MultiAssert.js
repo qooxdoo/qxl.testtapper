d339d4deabe71dde54ef7c0f99dfce75f80b7031
Henner Kollmann <henner.kollmann@gmx.de>
2020-01-12 15:19:04 +0100

    fix assert

--- source/class/qxl/testtapperdemo/test/Test02MultiAssert.js
+++ source/class/qxl/testtapperdemo/test/Test02MultiAssert.js
@@ -3,16 +3,16 @@ qx.Class.define("qxl.testtapperdemo.test.Test02MultiAssert", {
   members: {
     "test01: Encoding/Decoding": function() {
       var got = qx.util.Base64.decode(qx.util.Base64.encode("foo:bar")).split(":");
-      this.assertIdentical(got[0], "foo");
-      this.assertIdentical(got[1], "bar");
+      this.assertIdentical("foo", got[0]);
+      this.assertIdentical("bar", got[1]);
 
       got = qx.util.Base64.decode(qx.util.Base64.encode("foo:")).split(":");
-      this.assertIdentical(got[0], "foo");
-      this.assertIdentical(got[1], "");
+      this.assertIdentical("foo", got[0]);
+      this.assertIdentical(""   , got[1]);
 
       got = qx.util.Base64.decode(qx.util.Base64.encode("foo:" + null)).split(":");
-      this.assertIdentical(got[0], "foo");
-      this.assertIdentical(got[1], "null");
+      this.assertIdentical("foo" , got[0]);
+      this.assertIdentical("null", got[1]);
     }
   }
 
