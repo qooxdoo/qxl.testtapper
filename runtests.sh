9a2c052d98491bf77c8c8c85e55bc1306f5392a1
Henner Kollmann <henner.kollmann@gmx.de>
2020-01-14 22:10:42 +0100

    fix test

--- runtests.sh
+++ runtests.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-npx qx serve --machine-readable --target=source --listen-port=8089 &
-pid=$!
-while ! nc -z localhost 8089; do sleep 1; done
+#npx qx serve --machine-readable --target=source --listen-port=8089 &
+#pid=$!
+#while ! nc -z localhost 8089; do sleep 1; done
 #node compiled/source/resource/qxl/testtapper/run.js http://localhost:8089/testtapper/
 npx qx test
 if [ $? = 5 ]; then
     echo GOOD. Expected 5 tests to fail.
-    kill $pid
+#    kill $pid
     exit 0
 else
     echo BAD. Expected 5 tests to fail.    
-    kill $pid
+#    kill $pid
     exit 1
 fi
